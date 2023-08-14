8616c40d8e1bb7d6bbe799ac4de535f4276a1487
fbalhawan <balhawan.fouad@gmail.com>
2023-08-14 15:48:17 +0300

    Configured Sanity

---
+++ packages/frontend/sanity.config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./app/lib/sanity/schemas";
+import {
+  projectId,
+  dataset,
+  previewSecretId
+} from "./app/lib/sanity/config";
+import settings from "./app/lib/sanity/schemas/settings";
+import {
+  pageStructure,
+  // singletonPlugin
+} from "./app/lib/sanity/plugins/settings";
+import { unsplashImageAsset } from "sanity-plugin-asset-source-unsplash";
+import { table } from "@sanity/table";
+import { codeInput } from "@sanity/code-input";
+
+export const PREVIEWABLE_DOCUMENT_TYPES: string[] = ["post"];
+console.log(projectId);
+
+export default defineConfig({
+  name: "default",
+  title: "Fouad Blog",
+  basePath: "/studio",
+  projectId: projectId,
+  dataset: dataset,
+
+  plugins: [
+    deskTool({
+      structure: pageStructure([settings])
+      // `defaultDocumentNode` is responsible for adding a “Preview” tab to the document pane
+      // defaultDocumentNode: previewDocumentNode({ apiVersion, previewSecretId }),
+    }),
+    // singletonPlugin(["settings"]),
+    visionTool(),
+    unsplashImageAsset(),
+    table(),
+    codeInput()
+  ],
+
+  schema: {
+    types: schemaTypes
+  }
+});

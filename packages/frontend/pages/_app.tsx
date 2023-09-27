c4b78dcfb15e53b22fbd6a80dce048a7f09396e9
fbalhawan <balhawan.fouad@gmail.com>
2023-09-27 17:36:44 +0300

    Added google analytics

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -2,10 +2,12 @@ import type { AppProps } from 'next/app';
 import Layout from '../app/components/layout';
 import Link from 'next/link';
 import Head from 'next/head';
+import Script from 'next/script';
 import English from '../app/content/locales/en.json';
 import { useRouter } from 'next/router';
 import { useMemo } from 'react';
 import {IntlProvider, FormattedMessage, FormattedNumber} from 'react-intl'
+import Footer from 'components/footer';
 // export const metadata = {
 //   title: 'Welcome to my Portfolio',
 //   description: 'Hire a senior software engineer for your scalable backends',
@@ -39,6 +41,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <Script id="google-analytics">
+       {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TAG}');
+        `}
+      </Script>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TAG}`} />
     </IntlProvider>
   );
 }
